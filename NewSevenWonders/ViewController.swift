55dfc8927579e9f4e1121cfcfbe352df43230960
Robert Lin <linrobert0303@gmail.com>
2022-08-13 18:22:41 +0800

    Initial Commit

---
+++ NewSevenWonders/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewSevenWonders
+//
+//  Created by Robert Lin on 2022/8/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
